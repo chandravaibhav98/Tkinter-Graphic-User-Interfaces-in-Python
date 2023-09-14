2fe50d2b36ca0831576825e34afb2a64d1f748c1
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-09-14 17:03:27 +0530

    first mainloop()

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+root = Tk()
+# Creating a Label Widget
+
+myLabel = Label(root, text="tkinter")
+
+# Display it on screen
+myLabel.pack()
+
+root.mainloop()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
